88d1dfd36454d4c840b8851c64f0be5b1084c0e9
petetipaulkiran <petetipaulkiran@gmail.com>
2025-03-10 19:04:48 +0530

    Included the required url endipoints to develop the web application

--- backend/src/main/java/com/devsquad/backend/controller/UrlController.java
+++ backend/src/main/java/com/devsquad/backend/controller/UrlController.java
@@ -2,13 +2,38 @@ package com.devsquad.backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UrlController {
 
     @GetMapping("/")
-    public String check()
+    public String home()
     {
-        return "Welcome to the Application";
+        return "Home Page with a Navbar and Login/Signup Route";
+    }
+
+    @GetMapping("/login")
+    public String userlogin() {
+        return "Login Page which consists of username and password";
+    }
+
+    @GetMapping("/register")
+    public String userregister() {
+        return "User Registration Page with its fields";
+    }
+
+    @GetMapping("/home")
+    public String getMethodName() {
+       return "Home page of the user";
     }
     
+    @GetMapping("/shortenurl")
+    public String shorten_url() {
+        return "Page for Shortening the URL";
+    }
+    
+    @GetMapping("viewallurls")
+    public String viewallurls() {
+        return "display all the information about the urls based the username";
+    }
 }
