0a1a7baadec4845430e721772abb85bff0ac556a
petetipaulkiran <petetipaulkiran@gmail.com>
2025-03-11 17:52:58 +0530

    Seperated the Navbar from the App.jsx and Modified Home.jsx to work accordingly

--- url-shortener-frontend/src/App.js
+++ url-shortener-frontend/src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import User from "./components/User";
 import './App.css'; // Import the CSS file
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <div className="app-container">
-        <header className="app-header">
-          <h1 className="app-title">Zippy Link</h1>
-          <div className="auth-buttons">
-            <Link to="/login">
-              <button className="login-button">Login</button>
-            </Link>
-            <Link to="/signup">
-              <button className="signup-button">Sign Up</button>
-            </Link>
-          </div>
-        </header>
         <main className="app-main">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -33,12 +23,12 @@ const App = () => {
   );
 };
 
-const Home = () => {
-  return (
-    <div className="home-container">
-      <h1 className="home-title">URL Shortener</h1>
-    </div>
-  );
-};
+// const Home = () => {
+//   return (
+//     <div className="home-container">
+//       <h1 className="home-title">URL Shortener</h1>
+//     </div>
+//   );
+// };
 
 export default App;
